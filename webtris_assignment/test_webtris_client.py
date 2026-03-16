520292be0dec685f8ed13a737b56b159e386ec2d
Arav Anand <anand.ar@northeastern.edu>
2026-03-16 11:32:55 +0000

    Changed test file to correct name

--- webtris_assignment/test_webtris_client.py
+++ webtris_assignment/test_webtris_client.py
@@ -2,6 +2,11 @@ from datetime import date, time
 
 from webtris_client import TrafficObservation
 
+''' Traffic Observation Test '''
+def all_paramaters():
+    object1 = TrafficObservation(date(2026, 3, 3), time(0, 1, 30), "M25/4432A", 63.0, 100)
+    assert object1.has_all_the_data is True
+
 def test_traffic_observation():
     object1 = TrafficObservation(date(2026, 3, 3), time(0, 1, 30), "?", 65.0, 50)
     assert object1.has_all_the_data()
@@ -16,3 +21,4 @@ def test_traffic_observation_ordering():
     a = TrafficObservation(date(2026, 3, 3), time(0, 1, 30), "?", 65.0, 50)
     b = TrafficObservation(date(2026, 3, 3), time(0, 1, 30), "?", 65.0, 51)
     assert a < b
+
